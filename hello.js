ddda32d944d1f0f853c531312bd4c2e2549773ae
fikridzkr <fikridzakir179@gmail.com>
2021-03-11 20:30:18 +0700

    Hello Typescript

---
+++ hello.js
@@ -0,0 +1,5 @@
+function sayHello(name) {
+    return "hello :" + name;
+}
+var hello = sayHello("fikri");
+console.log(hello);
---
+++ hello.ts
@@ -0,0 +1,6 @@
+function sayHello(name: string) {
+  return `hello :${name}`;
+}
+
+let hello: string = sayHello("fikri");
+console.log(hello);
