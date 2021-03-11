c4fa4de999f219e96f504754927a4162a7b84cf9
fikridzkr <fikridzakir179@gmail.com>
2021-03-11 20:56:34 +0700

    primitive data types

--- hello.js
+++
@@ -1,5 +0,0 @@
-function sayHello(name) {
-    return "hello :" + name;
-}
-var hello = sayHello("fikri");
-console.log(hello);
---
+++ typedata.ts
@@ -0,0 +1,9 @@
+let isMarried: boolean = true;
+let year: number = 2021;
+let firstName: string = "fikri";
+let lastName: string = "dzakir";
+
+let fullName: string = firstName + "" + lastName;
+let helloWorld: string = `Hello ${fullName}`;
+
+console.log(helloWorld);
